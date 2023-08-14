156c3ea9c80910aa1a3e6f98f15b68815233916c
rianhidayat <hrian4182@gmail.com>
2023-08-14 14:11:38 +0700

    First Upload Folder

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/tugasHr/';
+$config['base_url'] = 'http://localhost/Profile-CompanyXYZ/';
 
 /*
 |--------------------------------------------------------------------------
